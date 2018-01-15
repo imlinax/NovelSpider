b6c82b9756c755dbce64c793b221725a4d463117
wangli <wangli.qufu@163.com>
2018-01-16 01:07:25 +0800

    parse form

--- src/golang/web_server/main.go
+++ src/golang/web_server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/golang/glog"
@@ -8,6 +9,21 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("www/")))
+	http.HandleFunc("/api/search", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "POST" {
+			w.WriteHeader(404)
+			fmt.Println("bad requst")
+		}
+		err := req.ParseForm()
+		if err != nil {
+			glog.Info(err)
+		}
+
+		novelName := req.FormValue("name")
+		fmt.Println("novel name: ", novelName)
+		w.Write([]byte(novelName))
+
+	})
 	err := http.ListenAndServe("0.0.0.0:80", nil)
 	if err != nil {
 		glog.Error(err)
