28c0b3a49eea0dcf130fe25850315bc8aa8b9468
wangli <wangli.qufu@163.com>
2018-01-17 23:48:39 +0800

    trigger novel crawler by web request

--- src/golang/web_server/main.go
+++ src/golang/web_server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/golang/glog"
 )
 
+const (
+	TOOLDIR = "tools"
+)
+
 func main() {
+	flag.Parse()
 	http.Handle("/", http.FileServer(http.Dir("www/")))
 	http.HandleFunc("/api/search", func(w http.ResponseWriter, req *http.Request) {
 		if req.Method != "POST" {
@@ -21,11 +29,22 @@ func main() {
 
 		novelName := req.FormValue("name")
 		fmt.Println("novel name: ", novelName)
-		w.Write([]byte(novelName))
+		w.Write([]byte("starting get " + novelName))
+		go crawlNovel(novelName)
 
 	})
 	err := http.ListenAndServe("0.0.0.0:80", nil)
 	if err != nil {
 		glog.Error(err)
 	}
+}
+
+func crawlNovel(name string) {
+	crawlerPath := filepath.Join(TOOLDIR, "crawl_novel")
+	cmd := exec.Command(crawlerPath, "-name="+name)
+	err := cmd.Run()
+	if err != nil {
+		glog.Error(err)
+	}
+	glog.Info("下载完成: ", name)
 }
